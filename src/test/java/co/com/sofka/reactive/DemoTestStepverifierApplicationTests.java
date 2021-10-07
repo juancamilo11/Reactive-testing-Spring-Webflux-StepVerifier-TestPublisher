187f86dc2d489953dd9ee632d6eca0b515de81de
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-10-06 22:19:05 -0500

    Se agrego la dependencia de spring-webflux

---
+++ src/test/java/co/com/sofka/reactive/DemoTestStepverifierApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestStepverifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
